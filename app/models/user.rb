fd319765eed6deea17f3eeb5486ae33d2e172622
Charles F. Munat <charles.munat@gmail.com>
2015-08-19 00:11:50 +1200

    Add a few comments to the model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,27 +1,37 @@
 class User
-  include MongoMapper::Document
+  include MongoMapper::Document # Make this a persistable Mongo model
   include BCrypt
 
+  # Run this method before creating new Users
   before_create :set_password
 
+  # Create a setter for the password so we can create
+  # new users by passing email and password
   attr_reader :password
 
   key :email, String
-  key :digest,  String
-  key :salt,  String
+  key :digest,  String    # Store the hashed password
+  key :salt,  String      # Store the salt used to hash the password
 
-  timestamps!
+  timestamps! # Create timestamps automatically
 
+  # Authenticate the user by passing this password
   def authenticate(password)
+    # Hash the passed in password
     digest = BCrypt::Engine.hash_secret(password, self.salt, 12)
+    # Compare the hashed password with the stored digest
+    # Returns true if authenticated, false if not
     digest == self.digest
   end
 
   private
 
   def set_password
+    # If a password has been passed in to #create
     if self.password
+      # Generate a salt
       self.salt = BCrypt::Engine.generate_salt
+      # Use the salt to hash the passed-in password and save the hash as digest
       self.digest = BCrypt::Engine.hash_secret(self.password, self.salt, 12)
     end
   end
