905f73945e586f7ebee2e84e69fa24f014d24196
Charles F. Munat <charles.munat@gmail.com>
2015-08-18 23:58:21 +1200

    Switch to rails form_for for login.

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,22 +1,31 @@
 class SessionController < ApplicationController
+  # redirect to the root page unless NOT logged in
   before_action :is_not_authenticated?, except: [ :destroy ]
 
+  # Display the login form
   def new
   end
 
+  # Log the user in
   def create
+    # Find the user
     if user = User.find_by_email(params[:user][:email])
+      # Test the user's password
       if user.authenticate(params[:user][:password])
+        # Log the user in and redirect to root page
         session[:user_id] = user.id
         redirect_to root_url
       else
+        # Bad password -- rerender the form
         render :new
       end
     else
+      # Can't find user -- rerender the form
       render :new
     end
   end
 
+  # Log the user out and redirect to the login form
   def destroy
     session[:user_id] = nil
     redirect_to login_url
@@ -25,6 +34,7 @@ class SessionController < ApplicationController
   private
 
   def is_not_authenticated?
+    # Redirect to the root page unless the user is NOT logged in
     redirect_to root_url unless session[:user_id].nil?
   end
 end
--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,15 +1,19 @@
 class SiteController < ApplicationController
+  # Redirect to login page unless logged in for home page only
   before_action :is_authenticated?, only: [ :index ]
 
+  # Home page (root)
   def index
   end
 
+  # Unprotected terms of use page
   def terms
   end
 
   private
 
   def is_authenticated?
+    # Redirect to login page unless logged in
     redirect_to login_url if session[:user_id].nil?
   end
 end
